c02def268fbc2f3ad9e92d0d651e7b195691aa60
DavidMcLaughlin55 <49570547+DavidMcLaughlin55@users.noreply.github.com>
2022-01-22 23:41:54 -0500

    JS Update-2

--- app.js
+++ app.js
@@ -1,34 +1,28 @@
-const express = require('express')
-const app = express()
-//const data = require('data.json')
-console.log(data)
-const port = 3000
+const express = require('express');
+//import JSON data
+const app = express();
+const port = 3000;
 
-app.set('view engine', 'pug') //sets "view engine to "Pug"
-app.use('/static', express.static('public')) //Serves the static files located in the 'public' folder
+app.set('view engine', 'pug'); //sets "view engine to "Pug"
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/static', express.static('public')); //Serves the static files located in the 'public' folder
 
 app.get('/', (req, res) => {
-    res.render('Home page!'); //An "index" route (/) to render the "Home" page with the locals set to data.projects
-})
+    console.log(data);
+    res.render('index');
+});
 
 app.get('/about', (req, res) => {
-    res.render("About page!"); //An "about" route (/about) to render the "About" page
-})
+    res.render('about');
+});
 
-app.use((req, res, next) => {
-    const err = new Error('Not Found');
-    err.status = 404;
-    next(err)
-})
-
-//Error when webpage not found
-app.use((err, req, res, next) => {
-    res.local.error = err;
-    res.status = err.status;
-    res.render('error', err);
-})
+app.get('/projects', (req, res) => {
+    res.render('project');
+});
 
 app.listen(port, () => {
     console.log(`This app is running on Port: ${port}`)
-})
+});
 
