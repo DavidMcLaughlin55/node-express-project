00755fbf525e3b4bad7a07f40619eeb8e3f700bd
DavidMcLaughlin55 <49570547+DavidMcLaughlin55@users.noreply.github.com>
2022-01-23 20:36:06 -0500

    JS Update-3
    JSON local data import no longer causing error.

--- app.js
+++ app.js
@@ -1,27 +1,29 @@
 const express = require('express');
-//import JSON data
 const app = express();
 const port = 3000;
+const projectsData = require('./data/data.json');
 
-app.set('view engine', 'pug'); //sets "view engine to "Pug"
+app.set('view engine', 'pug'); //sets view engine to "Pug"
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/static', express.static('public')); //Serves the static files located in the 'public' folder
 
+//Routes
 app.get('/', (req, res) => {
-    console.log(data);
-    res.render('index');
+    res.render('index', { projects: projectsData.projects });
 });
 
 app.get('/about', (req, res) => {
     res.render('about');
 });
 
-app.get('/projects', (req, res) => {
+app.get('/project', (req, res) => {
     res.render('project');
 });
 
+
+//Listener 
 app.listen(port, () => {
     console.log(`This app is running on Port: ${port}`)
 });
